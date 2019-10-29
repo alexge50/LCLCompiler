8136e09070a4e2dc0692c6bdb9993e7617f15ae3
LucaSas <sas.luca.alex@gmail.com>
2019-10-29 20:07:22 +0000

    Updated build files.

--- build.bat
+++ build.bat
@@ -2,11 +2,11 @@
 
 set include_directories=/I ../sources/ /I ../external/
 set disabled_warnings=/wd4458 /wd4201 /wd4189 /wd4100 /wd4505
-set common_compiler_flag=/nologo /MT /MP /std:c++17 /Gm- /Felcl /EHsc /EHa- /permissive- /Oi /Od /GR- /WX /W4 %disabled_warnings% /Z7
+set common_compiler_flag=/nologo /DEBUG:FULL /MT /MP /std:c++17 /Fe:lcl /Gm- /EHsc /EHa- /permissive- /Oi /Od /GR- /WX /W4 %disabled_warnings% /Z7
 
 if not exist bin mkdir bin
 pushd bin
 
-cl %common_compiler_flag% %include_directories% ../sources/*.cpp ../external/fmt/*.cc
+cl %common_compiler_flag% %include_directories% ../sources/*.cpp /link ../external_bin/format.lib ../external_bin/posix.lib ../external_bin/utf8proc.lib
 
 popd
--- test.bat
+++ test.bat
@@ -2,12 +2,12 @@
 
 set include_directories=/I ../sources/ /I ../external/ /I ../tests
 set disabled_warnings=/wd4458 /wd4201 /wd4189 /wd4100 /wd4505
-set common_compiler_flag=/nologo /MT /MP /std:c++17 /Gm- /EHsc /EHa- /permissive- /Oi /Od /GR- /WX /W4 %disabled_warnings% /Z7
+set common_compiler_flag=/nologo /DEBUG:FULL /MT /MP /std:c++17 /Fe:tokenizer_tests /Gm- /EHsc /EHa- /Z7 /permissive- /Oi /Od /GR- /WX /W4 %disabled_warnings%
 
 if not exist tests_bin mkdir tests_bin
 pushd tests_bin
 
-cl %common_compiler_flag% /Fe:tokenizer_tests %include_directories% ../sources/tokenizer.cpp ../tests/test_tokenizer.cpp
+cl %common_compiler_flag% %include_directories% ../tests/test_tokenizer.cpp ../sources/tokenizer.cpp /link ../external_bin/format.lib ../external_bin/posix.lib ../external_bin/utf8proc.lib
 
 popd
 
