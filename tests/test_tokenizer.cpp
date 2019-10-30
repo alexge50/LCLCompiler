4f7e3239e0b55aa2007ac86d731b7b346c5d5419
LucaSas <sas.luca.alex@gmail.com>
2019-10-30 01:43:57 +0000

    Converted tests to catch2 library.

--- tests/test_tokenizer.cpp
+++ tests/test_tokenizer.cpp
@@ -1,214 +1,284 @@
-#include <cassert>
-
-#include <fmt/core.h>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
 #include <tokenizer.hpp>
 #include <std_utils.hpp>
 
 using namespace std::string_view_literals;
 
-void test_tokenization_of_whitespace()
+TEST_CASE("Tokenization of whitespace", "[tokenizer]")
 {
-    const auto source = "    \r\t"sv;
-    const auto result = lcl::tokenize_code(source);
+    const auto code = "    \r\t"sv;
+    const auto result = lcl::tokenize_code(code);
  
-    assert(result.size() == 0);
+    REQUIRE(result.size() == 0);    
 }
 
-void test_tokenization_of_single_line_comment()
+TEST_CASE("Tokenization of single line comment", "[tokenizer]")
 {
-    const auto source = "////Test漢語"sv;
-    const auto result = lcl::tokenize_code(source);
- 
-    assert(result.size() == 1);
-
-    assert(result[0].type        == lcl::token_type::comment);
-    assert(result[0].code.size() == source.size());
-    assert(result[0].code        == source);
-}
-
-void test_tokenization_of_multi_line_comment()
-{
+    SECTION("Empty comment")
     {
-        const auto source = "/* //Test漢語 /* inner comment */ \n\n\n */"sv;
-        const auto result = lcl::tokenize_code(source);
+        const auto code = "//"sv;
+        const auto result = lcl::tokenize_code(code);
     
-        assert(result.size() == 1);
+        REQUIRE(result.size() == 1);
 
-        assert(result[0].type        == lcl::token_type::comment);
-        assert(result[0].code.size() == source.size());
-        assert(result[0].code        == source);
-        assert(result[0].is_multi_line_comment());
+        REQUIRE(result[0].type == lcl::token_type::comment);
+        REQUIRE(result[0].code == code);
     }
 
+    SECTION("Comment with text")
     {
-        const auto source = "/* //Test漢語 /* inner comment */ // // /* // */\n\n\n */"sv;
-        const auto result = lcl::tokenize_code(source);
+        const auto code = "//Test"sv;
+        const auto result = lcl::tokenize_code(code);
+
+        REQUIRE(result.size() == 1);
+
+        REQUIRE(result[0].type == lcl::token_type::comment);
+        REQUIRE(result[0].code == code);
+    }
+
+    SECTION("Comment with extra forward slashes")
+    {
+        const auto code = "////Test"sv;
+        const auto result = lcl::tokenize_code(code);
+
+        REQUIRE(result.size() == 1);
+
+        REQUIRE(result[0].type == lcl::token_type::comment);
+        REQUIRE(result[0].code == code);
+    }
+
+    SECTION("Comment with weird characters")
+    {
+        const auto code = "////Test漢語"sv;
+        const auto result = lcl::tokenize_code(code);
+
+        REQUIRE(result.size() == 1);
+
+        REQUIRE(result[0].type == lcl::token_type::comment);
+        REQUIRE(result[0].code == code);
+    }
+
+    SECTION("Single line comment with multi line comment opener")
+    {
+        const auto code = "////Test漢語 /* "sv;
+        const auto result = lcl::tokenize_code(code);
+
+        REQUIRE(result.size() == 1);
+
+        REQUIRE(result[0].type == lcl::token_type::comment);
+        REQUIRE(result[0].code == code);
+    }
+
+    SECTION("Two single line comments")
+    {
+        const auto code                = "//Test \n //Test"sv;
+        const auto first_comment_code  = "//Test "sv;
+        const auto second_comment_code = "//Test"sv;
+
+        const auto result = lcl::tokenize_code(code);
+
+
+        REQUIRE(result.size() == 2);
+
+        REQUIRE(result[0].type == lcl::token_type::comment);
+        REQUIRE(result[0].code == first_comment_code);
+
+        REQUIRE(result[1].type == lcl::token_type::comment);
+        REQUIRE(result[1].code == second_comment_code);
+    }
+}
+
+TEST_CASE("Tokenization of multi line comment", "[tokenizer]")
+{
+    SECTION("Multi line comment with no text")
+    {
+        const auto code = "/* \n\n\n */"sv;
+        const auto result = lcl::tokenize_code(code);
     
-        assert(result.size() == 1);
+        REQUIRE(result.size() == 1);
 
-        assert(result[0].type        == lcl::token_type::comment);
-        assert(result[0].code.size() == source.size());
-        assert(result[0].code        == source);
-        assert(result[0].is_multi_line_comment());
+        REQUIRE(result[0].type == lcl::token_type::comment);
+        REQUIRE(result[0].code == code);
+        REQUIRE(result[0].is_multi_line_comment());
     }
 
+    SECTION("Multi line comment with a regular nested comment")
     {
-        const auto source = "//"sv;
-        const auto result = lcl::tokenize_code(source);
+        const auto code = "/* //Test漢語 \n\n\n */"sv;
+        const auto result = lcl::tokenize_code(code);
     
-        assert(result.size() == 1);
+        REQUIRE(result.size() == 1);
 
-        assert(result[0].type        == lcl::token_type::comment);
-        assert(result[0].code.size() == source.size());
-        assert(result[0].code        == source);
+        REQUIRE(result[0].type == lcl::token_type::comment);
+        REQUIRE(result[0].code == code);
+        REQUIRE(result[0].is_multi_line_comment());
+    }
+
+    SECTION("Multi line comment with nested multi line comments")
+    {
+        const auto code = "/* //Test漢語 /* inner comment */ // // /* // */\n\n\n */"sv;
+        const auto result = lcl::tokenize_code(code);
+    
+        REQUIRE(result.size() == 1);
+
+        REQUIRE(result[0].type == lcl::token_type::comment);
+        REQUIRE(result[0].code == code);
+        REQUIRE(result[0].is_multi_line_comment());
     }
 }
 
-void test_tokenization_of_string()
+TEST_CASE("Tokenization of string", "[tokenizer]")
 {
-    const auto source = "\"Test\""sv;
-    const auto result = lcl::tokenize_code(source);
+    const auto code = "\"Test\""sv;
+    const auto result = lcl::tokenize_code(code);
 
-    assert(result.size() == 1);
-    assert(result[0].type        == lcl::token_type::string_literal);
-    assert(result[0].code.size() == source.size());
-    assert(result[0].code        == source);
-    assert(result[0].is_string_literal());
+    REQUIRE(result.size() == 1);
+
+    REQUIRE(result[0].type == lcl::token_type::string_literal);
+    REQUIRE(result[0].code == code);
+    REQUIRE(result[0].is_string_literal());
 }
 
-void test_tokenization_of_numeric_literals()
+TEST_CASE("Tokenization of numeric literals", "[tokenizer]")
 {
+    SECTION("Single numeric literal")
     {
-        const auto source = "1801"sv;
-        const auto result = lcl::tokenize_code(source);
+        const auto code = "1801"sv;
+        const auto result = lcl::tokenize_code(code);
 
-        assert(result.size() == 1);
-        assert(result[0].type        == lcl::token_type::numeric_literal);
-        assert(result[0].code.size() == source.size());
-        assert(result[0].code        == source);
-        assert(result[0].is_numeric_literal());
-        assert(result[0].is_int_literal());
+        REQUIRE(result.size() == 1);
+
+        REQUIRE(result[0].type == lcl::token_type::numeric_literal);
+        REQUIRE(result[0].code == code);
+        REQUIRE(result[0].is_numeric_literal());
+        REQUIRE(result[0].is_int_literal());
     }
 
+    SECTION("Two numeric literals")
     {
-        const auto source = "1801 83274"sv;
-        const auto result = lcl::tokenize_code(source);
+        const auto code = "1801 83274"sv;
+        const auto result = lcl::tokenize_code(code);
 
-        assert(result.size() == 2);
+        REQUIRE(result.size() == 2);
         
-        assert(result[0].type        == lcl::token_type::numeric_literal);
-        assert(result[0].code.size() == "1801"sv.size());
-        assert(result[0].code        == "1801"sv);
-        assert(result[0].is_numeric_literal());
-        assert(result[0].is_int_literal());
+        REQUIRE(result[0].type == lcl::token_type::numeric_literal);
+        REQUIRE(result[0].code == "1801"sv);
+        REQUIRE(result[0].is_numeric_literal());
+        REQUIRE(result[0].is_int_literal());
 
-        assert(result[1].type        == lcl::token_type::numeric_literal);
-        assert(result[1].code.size() == "83274"sv.size());
-        assert(result[1].code        == "83274"sv);
-        assert(result[0].is_numeric_literal());
-        assert(result[1].is_int_literal());
+        REQUIRE(result[1].type == lcl::token_type::numeric_literal);
+        REQUIRE(result[1].code == "83274"sv);
+        REQUIRE(result[1].is_numeric_literal());
+        REQUIRE(result[1].is_int_literal());
     }
 
+    SECTION("Numeric literal with underscore")
     {
-        const auto source = "1801_83274"sv;
-        const auto result = lcl::tokenize_code(source);
+        const auto code = "1801_83274"sv;
+        const auto result = lcl::tokenize_code(code);
 
-        assert(result.size() == 1);
+        REQUIRE(result.size() == 1);
         
-        assert(result[0].type        == lcl::token_type::numeric_literal);
-        assert(result[0].code.size() == "1801_83274"sv.size());
-        assert(result[0].code        == "1801_83274"sv);
-        assert(result[0].is_numeric_literal());
-        assert(result[0].is_int_literal());
+        REQUIRE(result[0].type == lcl::token_type::numeric_literal);
+        REQUIRE(result[0].code == "1801_83274"sv);
+        REQUIRE(result[0].is_numeric_literal());
+        REQUIRE(result[0].is_int_literal());
     }
 
+    SECTION("Floating point literal")
     {
-        const auto source = "1801.83274"sv;
-        const auto result = lcl::tokenize_code(source);
+        const auto code = "1801.83274"sv;
+        const auto result = lcl::tokenize_code(code);
 
-        assert(result.size() == 1);
+        REQUIRE(result.size() == 1);
         
-        assert(result[0].type        == lcl::token_type::numeric_literal);
-        assert(result[0].code.size() == "1801.83274"sv.size());
-        assert(result[0].code        == "1801.83274"sv);
-        assert(result[0].is_numeric_literal());
-        assert(result[0].is_float_literal());
+        REQUIRE(result[0].type == lcl::token_type::numeric_literal);
+        REQUIRE(result[0].code == "1801.83274"sv);
+        REQUIRE(result[0].is_numeric_literal());
+        REQUIRE(result[0].is_float_literal());
     }
 }
 
-void test_tokenization_of_single_char_tokens()
+TEST_CASE("Tokenization of single char tokens", "[tokenizer]")
 {
-    //Test sources with single char
-    for (const auto source : lcl::single_char_tokens)
+    SECTION("Single char tokens")
     {
-        const auto result = lcl::tokenize_code(source);
-
-        assert(result.size() == 1);
-        
-        assert(result[0].type        == lcl::get_token_type_that_represents_char(source[0]));
-        assert(result[0].code.size() == 1);
-        assert(result[0].code        == source);
-        assert(result[0].is_single_char_token());
-    }
-
-    //Test sources with multiple chars of the same type
-    for (const auto it : lcl::single_char_tokens)
-    {
-        //Skip this one because `//` is a comment.
-        if (it == "/")
+        for (const auto code : lcl::single_char_tokens)
         {
-            continue;
+            const auto result = lcl::tokenize_code(code);
+
+            REQUIRE(result.size() == 1);
+            
+            REQUIRE(result[0].type        == lcl::get_token_type_that_represents_char(code[0]));
+            REQUIRE(result[0].code.size() == 1);
+            REQUIRE(result[0].code        == code);
+            REQUIRE(result[0].is_single_char_token());
         }
+    }
 
-        const auto source = std::string { it } + std::string { it } + std::string { it };
-        const auto result = lcl::tokenize_code(source);
-
-        assert(result.size() == 3);
-        
-        for (auto i = 0; i < lcl::utils::ssize(result); ++i)
+    //Test codes with multiple chars of the same type
+    SECTION("Single char tokens repeated")
+    {
+        for (const auto it : lcl::single_char_tokens)
         {
-            assert(result[i].type        == lcl::get_token_type_that_represents_char(source[0]));
-            assert(result[i].code.size() == 1);
-            assert(result[i].code        == it);
-            assert(result[i].is_single_char_token());
+            //Skip this one because `//` is a comment.
+            if (it == "/")
+            {
+                continue;
+            }
+
+            const auto code = std::string { it } + std::string { it } + std::string { it };
+            const auto result = lcl::tokenize_code(code);
+
+            REQUIRE(result.size() == 3);
+            
+            for (auto i = 0; i < lcl::utils::ssize(result); ++i)
+            {
+                REQUIRE(result[i].type == lcl::get_token_type_that_represents_char(code[0]));
+                REQUIRE(result[i].code == it);
+                REQUIRE(result[i].is_single_char_token());
+            }
         }
     }
 }
 
-void test_tokenization_of_words()
+TEST_CASE("Tokenization of words", "[tokenizer]")
 {
+    SECTION("Single word")
     {
-        const auto source = "test";
+        const auto code = "test";
 
-        const auto result = lcl::tokenize_code(source);
+        const auto result = lcl::tokenize_code(code);
 
-        assert(result.size() == 1);
+        REQUIRE(result.size() == 1);
         
-        assert(result[0].type        == lcl::token_type::word);
-        assert(result[0].code.size() == 1);
-        assert(result[0].code        == source);
-        assert(result[0].is_word());
-        assert(result[0].is_identifier());
+        REQUIRE(result[0].type == lcl::token_type::word);
+        REQUIRE(result[0].code == code);
+        REQUIRE(result[0].is_word());
+        REQUIRE(result[0].is_identifier());
     }
 
-    for (const auto source : lcl::keywords)
+    SECTION("All keywords")
     {
-        const auto result = lcl::tokenize_code(source);
+        for (const auto code : lcl::keywords)
+        {
+            const auto result = lcl::tokenize_code(code);
 
-        assert(result.size() == 1);
-        
-        assert(result[0].type        == lcl::token_type::word);
-        assert(result[0].code.size() == 1);
-        assert(result[0].code        == source);
-        assert(result[0].is_word());
-        assert(result[0].is_keyword());
+            REQUIRE(result.size() == 1);
+            
+            REQUIRE(result[0].type == lcl::token_type::word);
+            REQUIRE(result[0].code == code);
+            REQUIRE(result[0].is_word());
+            REQUIRE(result[0].is_keyword());
+        }
     }
 }
 
-void test_tokenization_of_complex_example()
+TEST_CASE("Tokenization of complex example", "[tokenizer]")
 {
-    const auto source = R"source_code(
+    const auto code = R"code_code(
         import Print: *;
 
         main :: () -> void
@@ -220,7 +290,7 @@ void test_tokenization_of_complex_example()
                 print("Hello Sailor!");
             }
         }
-    )source_code";
+    )code_code";
 
     const auto expected_token_types = lcl::utils::make_array
     (
@@ -237,57 +307,48 @@ void test_tokenization_of_complex_example()
         lcl::token_type::close_curly
     );
 
-    const auto result = lcl::tokenize_code(source);
+    const auto result = lcl::tokenize_code(code);
 
-    assert(result.size() == expected_token_types.size());
-
-    const auto tokens_as_expected = [&] ()
+    SECTION("Tokens match")
     {
-        for (auto i = 0; i < lcl::utils::ssize(result); ++i)
+        REQUIRE(result.size() == expected_token_types.size());
+
+        const auto tokens_as_expected = [&] ()
         {
-            if (result[i].type != expected_token_types[i])
+            for (auto i = 0; i < lcl::utils::ssize(result); ++i)
             {
-                return false;
+                if (result[i].type != expected_token_types[i])
+                {
+                    return false;
+                }
             }
-        }
 
-        return true;
-    }();
+            return true;
+        }();
 
-    assert(tokens_as_expected);
+        REQUIRE(tokens_as_expected);
+    }
 
-    for (auto token_index = 0; token_index < lcl::utils::ssize(result); ++token_index)
+    SECTION("Test words match")
     {
-        const auto& token = result[token_index];
-
-        if (token.is_word())
+        for (auto token_index = 0; token_index < lcl::utils::ssize(result); ++token_index)
         {
-            switch (token_index)
+            const auto& token = result[token_index];
+
+            if (token.is_word())
             {
-                case  0: assert(token.is_keyword()    && token.code == "import"); break;
-                case  2: assert(token.is_identifier() && token.code == "Print");  break;
-                case  6: assert(token.is_identifier() && token.code == "main");   break;
-                case 13: assert(token.is_keyword()    && token.code == "void");   break;
-                case 15: assert(token.is_identifier() && token.code == "hello");  break;
-                case 20: assert(token.is_keyword()    && token.code == "while");  break;
-                case 22: assert(token.is_identifier() && token.code == "hello");  break;
-                case 28: assert(token.is_identifier() && token.code == "print");  break;
+                switch (token_index)
+                {
+                    case  0: REQUIRE(token.is_keyword());    REQUIRE(token.code == "import"); break;
+                    case  2: REQUIRE(token.is_identifier()); REQUIRE(token.code == "Print");  break;
+                    case  6: REQUIRE(token.is_identifier()); REQUIRE(token.code == "main");   break;
+                    case 13: REQUIRE(token.is_keyword());    REQUIRE(token.code == "void");   break;
+                    case 15: REQUIRE(token.is_identifier()); REQUIRE(token.code == "hello");  break;
+                    case 20: REQUIRE(token.is_keyword());    REQUIRE(token.code == "while");  break;
+                    case 22: REQUIRE(token.is_identifier()); REQUIRE(token.code == "hello");  break;
+                    case 28: REQUIRE(token.is_identifier()); REQUIRE(token.code == "print");  break;
+                }
             }
         }
     }
-}
-
-int main()
-{
-    fmt::print("Running Tokenizer Tests\n");
-    
-    test_tokenization_of_whitespace();
-    test_tokenization_of_single_line_comment();
-    test_tokenization_of_multi_line_comment();
-    test_tokenization_of_string();
-    test_tokenization_of_single_char_tokens();
-    test_tokenization_of_numeric_literals();
-    test_tokenization_of_complex_example();
-    
-    fmt::print("Tokenizer tests run succesfully.");
 }
