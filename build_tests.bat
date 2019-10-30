7095a227baa4a961df8390c63eb8487db4d8972c
LucaSas <sas.luca.alex@gmail.com>
2019-10-30 14:13:28 +0000

    Added rangesv3 to tests build file.

--- build_tests.bat
+++ build_tests.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set include_directories=/I ../../sources/ /I ../../libs/ /I ../../tests
+set include_directories=/I ../../sources/ /I ../../libs/ /I ../../libs/rangesv3 /I ../../tests
 set disabled_warnings=/wd4458 /wd4201 /wd4189 /wd4100 /wd4505
 set common_compiler_flags=/nologo /DEBUG:FULL /MT /MP /std:c++17 /Fe:tokenizer_tests /Gm- /EHsc /EHa- /Z7 /permissive- /Oi /Od /GR- /WX /W4 %disabled_warnings%
 set libraries=../libs_bin/format.lib ../libs_bin/posix.lib ../libs_bin/utf8proc.lib
